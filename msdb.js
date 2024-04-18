da5db2bc12bac6b59e6d0067783352ad7211b5eb
Alone and Lonely dev <golfdx34@gmail.com>
2024-04-18 08:33:01 +0700

    Update msdb.js

--- msdb.js
+++ msdb.js
@@ -1,105 +1,149 @@
 const fs = require('fs');
 const path = require('path');
 
+/**
+ * Creates and initializes a database with the given name.
+ * @param {string} databaseName - the name of the database to create
+ * @returns {function} - a function that creates and initializes a table in the database
+ */
 function initializeDatabase(databaseName) {
-    const databaseFolderPath = './MakiShop_Database';
-    if (!fs.existsSync(databaseFolderPath)) {
-        fs.mkdirSync(databaseFolderPath);
+  const databaseFolderPath = './Database';
+  if (!fs.existsSync(databaseFolderPath)) {
+    fs.mkdirSync(databaseFolderPath);
+  }
+  const fullDatabaseFolderPath = path.join(databaseFolderPath, databaseName);
+  if (!fs.existsSync(fullDatabaseFolderPath)) {
+    fs.mkdirSync(fullDatabaseFolderPath);
+  }
+
+  return function initializeTable(tableName) {
+    const tableFilename = path.join(fullDatabaseFolderPath, `${tableName}.json`);
+    let tableData = {};
+
+    if (fs.existsSync(tableFilename)) {
+      const data = fs.readFileSync(tableFilename, 'utf8');
+      tableData = JSON.parse(data);
     }
 
-    const fullDatabaseFolderPath = path.join(databaseFolderPath, databaseName);
-    if (!fs.existsSync(fullDatabaseFolderPath)) {
-        fs.mkdirSync(fullDatabaseFolderPath);
+    /**
+     * Saves an entry to the table.
+     * @param {string} id - the unique ID of the entry
+     * @param {object} data - the data to save
+     */
+    function saveTable(id, data) {
+      const entryId = id || generateUniqueId();
+      tableData[entryId] = {
+        id: entryId,
+        value: data.value || data || null,
+      };
+
+      const jsonData = JSON.stringify(tableData, null, 2);
+      fs.writeFileSync(tableFilename, jsonData, 'utf8');
     }
 
-    return function initializeTable(tableName) {
+    /**
+     * Updates the table data in the file system.
+     */
+    function updateJson() {
+      const jsonData = JSON.stringify(tableData, null, 2);
+      fs.writeFileSync(tableFilename, jsonData, 'utf8');
+    }
 
-        const tableFilename = path.join(fullDatabaseFolderPath, `${tableName}.json`);
-        let tableData = {};
+    /**
+     * Removes an entry from the table.
+     * @param {string} key - the unique ID of the entry to remove
+     */
+    function removeFromTable(key) {
+      if (tableData.hasOwnProperty(key)) {
+        delete tableData[key];
+        updateJson();
+      }
+    }
 
-        if (fs.existsSync(tableFilename)) {
-            const data = fs.readFileSync(tableFilename, 'utf8');
-            tableData = JSON.parse(data);
+    /**
+     * Adds an entry to the table.
+     * @param {string} id - the unique ID of the entry
+     * @param {object} data - the data to add
+     */
+    function addToTable(id, data) {
+      saveTable(id, data);
+    }
+
+    /**
+     * Retrieves an entry from the table.
+     * @param {string} key - the unique ID of the entry
+     * @returns {object} - the entry with the given ID, or null if no entry with the given ID exists
+     */
+    function getFromTable(key) {
+      return tableData[key];
+    }
+
+    /**
+     * Retrieves a random entry from the table.
+     * @returns {object} - a random entry from the table, or null if the table is empty
+     */
+    function getRandomEntry() {
+      const keys = Object.keys(tableData);
+      if (keys.length === 0) {
+        return null;
+      }
+      const randomKey = keys[Math.floor(Math.random() * keys.length)];
+      return tableData[randomKey];
+    }
+
+    /**
+     * Retrieves all entries from the table.
+     * @param {string} orderBy - the order to return the entries in ('asc' for ascending, 'desc' for descending)
+     * @returns {object[]} - an array of all entries in the table, sorted according to the given order
+     */
+    function getAllEntries(orderBy = 'asc') {
+      const entries = Object.values(tableData);
+
+      if (orderBy === 'desc') {
+        entries.sort((a, b) => (a.id > b.id ? -1 : 1));
+      } else {
+        entries.sort((a, b) => (a.id > b.id ? 1 : -1));
+      }
+      return entries;
+    }
+
+    /**
+     * Retrieves entries that match the given condition.
+     * @param {object} condition - the condition to match
+     * @returns {object[]} - an array of entries that match the given condition
+     */
+    function getWhere(condition) {
+      const entries = Object.entries(tableData);
+      const results = [];
+
+      for (const [key, value] of entries) {
+        if (value.value && value.value.good == condition.good) {
+          results.push(value.value);
         }
+      }
 
-        function saveTable(id, data) {
-            const entryId = id || generateUniqueId();
-            tableData[entryId] = {
-                id: entryId,
-                value: data.value || data || null,
-            };
-
-            const jsonData = JSON.stringify(tableData, null, 2);
-            fs.writeFileSync(tableFilename, jsonData, 'utf8');
-        }
-        function updateJson() {
-            const jsonData = JSON.stringify(tableData, null, 2);
-            fs.writeFileSync(tableFilename, jsonData, 'utf8');
-        }
-        function removeFromTable(key) {
-            if (tableData.hasOwnProperty(key)) {
-                delete tableData[key];
-                updateJson()
-            }
-        }
-
-
-        function addToTable(id, data) {
-            saveTable(id, data);
-        }
-
-        function getFromTable(key) {
-            return tableData[key];
-        }
+      return results;
+    }
 
 
 
-        function getRandomEntry() {
-            const keys = Object.keys(tableData);
-            if (keys.length === 0) {
-                return null;
-            }
-            const randomKey = keys[Math.floor(Math.random() * keys.length)];
-            return tableData[randomKey];
-        }
-
-        function getAllEntries(orderBy = 'asc') {
-            const entries = Object.values(tableData);
-
-            if (orderBy === 'desc') {
-                entries.sort((a, b) => (a.id > b.id ? -1 : 1));
-            } else {
-                entries.sort((a, b) => (a.id > b.id ? 1 : -1));
-            }
-
-            return entries;
-        }
-
-        function getWhere(condition) {
-            const entries = Object.values(tableData);
-            return entries.filter(entry => {
-                for (const key in condition) {
-                    if (entry[key] !== condition[key]) {
-                        return false;
-                    }
-                }
-                return true;
-            });
-        }
-
-        return {
-            find: getFromTable,
-            save: addToTable,
-            remove: removeFromTable,
-            random: getRandomEntry,
-            getAll: getAllEntries,
-            getWhere: getWhere,
-        };
+    return {
+      find: getFromTable,
+      save: addToTable,
+      remove: removeFromTable,
+      random: getRandomEntry,
+      getAll: getAllEntries,
+      getWhere: getWhere
     };
+  };
 }
 
+/**
+ * Generates a unique ID.
+ * @returns {string} - a unique ID
+ */
 function generateUniqueId() {
-    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 }
 
 module.exports = initializeDatabase;
